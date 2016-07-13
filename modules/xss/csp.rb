d6346e40e905bf78dc253752dee2a87b84d078db
Karthik Krishnan <karthikvkrishnan@gmail.com>
2016-07-13 15:05:34 +0530

    Minor Bug fix

--- modules/xss/csp.rb
+++ modules/xss/csp.rb
@@ -1,5 +1,9 @@
 module XSS
   module Csp
+
+    def self.implement_protection_strategy_in_request(instance)
+    end
+
     def self.implement_protection_strategy_in_response(instance)
       instance.instance_eval do
         headers["Content-Security-Policy"] = "default-src 'self' http://www.w3schools.com;"
